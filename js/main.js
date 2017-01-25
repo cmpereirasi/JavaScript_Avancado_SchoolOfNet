55b6febce7985bdbd63dd70e671b0811bed8b6c6
cmpereira <cmpereira50@gmail.com>
2017-01-25 16:37:49 -0200

    function formatDesc for treatment of letters uppercase

--- js/main.js
+++ js/main.js
@@ -14,10 +14,15 @@ function getTotal(list){
 function setList(list){
 	var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
 	for(var key in list){
-		table += '<tr><td>'+list[key].desc+'</td><td>'+list[key].amount+'</td><td>'+list[key].value+'</td><td>Edit | Delete</td></tr>';
+		table += '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+list[key].amount+'</td><td>'+list[key].value+'</td><td>Edit | Delete</td></tr>';
 		table +='</tbody>';
 		document.getElementById("listTable").innerHTML = table;
 	}
+}
+function formatDesc(desc){
+	var str = desc.toLowerCase();
+	str = str.charAt(0).toUpperCase() + str.slice(1);
+	return str;
 }
 setList(list)
 console.log(getTotal(list));
