f3bccd5e2b0b1f740735615ad55294f094e79a84
cmpereira <cmpereira50@gmail.com>
2017-02-18 11:23:07 -0200

    function dete record or element

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function getTotal(list){
 function setList(list){
 	var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
 	for(var key in list){
-		table += '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+list[key].amount+'</td><td>'+formatValue(list[key].value)+'</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> Delete</td></tr>';
+		table += '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+list[key].amount+'</td><td>'+formatValue(list[key].value)+'</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> <button class="btn btn-default" onclick="deleteData('+key+');">Delete</button></td></tr>';
 		table +='</tbody>';
 		document.getElementById("listTable").innerHTML = table;
 	}
@@ -70,6 +70,22 @@ function updateDate(){
 	list[id] = {"desc":desc, "amount":amount, "value":value};
 	resetForm();
 	setList(list);
+}
+
+// Delete data in my list
+function deleteData(id){
+	if(confirm("Delete this item?")){
+		if(id === list.length - 1){// last element
+			list.pop(); // clear last element in vector or list
+		}else if(id === 0){
+			list.shift(); // clear fist element in vector or list
+		}else{
+			var arrAuxIni = list.slice(0,id);
+			var arrAuxEnd = list.slice(id + 1);
+			list = arrAuxIni.concat(arrAuxEnd);
+		}
+		setList(list);
+	}
 }
 
 setList(list)
